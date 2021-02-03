c12abcbee59269750f1325364316d040acab140c
cmendoza <clrwxb@gmail.com>
2021-02-03 11:15:16 -0800

    continuing work on displayQuotes functionality and condition logic

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // Global Variables
+const apiURL = "https://swapi.dev/api/"
 
 // Select Elements (caching)
 const globeEl = document.querySelector("#globe")
@@ -9,11 +10,11 @@ const planetsEl = document.querySelector("#planet-facts")
 const filmDataEl = document.querySelector("#film-data")
 const imgs = document.querySelectorAll(".pic")
 const titleDataEl = document.querySelector("#image-caption")
-const apiURL = "https://swapi.dev/api/"
+
+
 
 
 // Event Listeners
-
 
 fetch(apiURL + "planets")
   .then((responseData) => {
@@ -31,28 +32,20 @@ fetch(apiURL + "planets")
       quotesEl.appendChild(pEl)
     }
 
-
+      
     for (let i = 0; i < imgs.length; i++) {
-      imgs[i].addEventListener("click", function () {
-        if (imgs[i].getAttribute("planet") === "Tatooine") {
+      imgs[i].addEventListener("click", function (e) {
+        let planet = e.target.getAttribute("planet")
+        if (planet === "Tatooine") {
           console.log("inside: ", this.getAttribute("planet"))
-          titleDataEl.textContent = `Welcome to Tatooine! Also known as the planet of the twin suns. This is the home planet of the Skywalker's!`
-          planetsEl.textContent = `Here are some interesting facts about this planet: 
-            Terrain: ${parsedData.results[i].terrain}
-            Population: ${parsedData.results[i].population}
-            Diameter: ${parsedData.results[i.diameter]}
-            `
-        } else if (imgs[i].getAttribute("planet") === "Hoth") {
+          titleDataEl.textContent = `Welcome to Tatooine! Also known as the planet of the twin suns. This is the home planet of the Skywalker's!`          
+        } else if (planet === "Hoth") {
           console.log("inside: ", this.getAttribute("planet"))
           titleDataEl.textContent = "Welcome to Hoth! This planet made it's debut in the second installment of the original star wars trilogy, known for its freezing cold weather, beautiful views and Taun-Tauns!"
         } else {
           console.log("inside: ", this.getAttribute("planet"))
           titleDataEl.textContent = "Welcome to Endor! Home of tall trees, tree-house structures, and of course home of the furry Ewok's!"
-          planetsEl.textContent = `Here are some interesting facts about this planet: 
-            Terrain: ${parsedData.results[i].terrain}
-            Population: ${parsedData.results[i].population}
-            Diameter: ${parsedData.results[i.diameter]}
-            `
+          
         }
       })
     }
@@ -64,6 +57,10 @@ fetch(apiURL + "planets")
 
   })
 
+  for(let i = 0; i < imgs.length; i++) {
+    imgs[i].addEventListener("click", planetWeather)
+  }
+  
 
 
 // Functions
@@ -80,12 +77,14 @@ const displayPlanetData = () => {
   }
 } 
 
-document.querySelector("#thumbs").addEventListener("click", () => {
-
-  let timer = null
-  let index = 0
+document.querySelector("#thumbs").addEventListener("click", (e) => {
 
   function displayQuote() {
+
+    let timer = null
+    let index = 0
+    const planet = e.target.getAttribute("planet")
+
     const newHopequotes = ["That’s no moon. It’s a space station. — Obi-Wan Kenobi", "Aren’t you a little short for a stormtrooper? — Leia Organa", "I find your lack of faith disturbing. — Darth Vader"]
 
     const empireQuotes = ["Yes, Your Highnessness? – Han Solo", "He’s no good to me dead. – Boba Fett", "The son of Skywalker must not become a Jedi. – Emperor Palpatine",
@@ -93,35 +92,43 @@ document.querySelector("#thumbs").addEventListener("click", () => {
 
     const returnQuotes = ["Bring me Solo and the Wookiee. They will all suffer for this outrage.", "Obi-Wan has taught you well.", "And now, young Skywalker...you will die.", "IT'S A TRAP!!", "You cannot hide forever, Luke.", "I am a Jedi, like my father before me", "What is thy bidding, my master?"]
 
-
-
-    // for(let idx in newHopequotes) {
-    //   quotesEl.innerHTML = newHopequotes[idx]
-    //   console.log(index)
-    //   // console.log(index)
-    // }
     quotesEl.innerHTML = newHopequotes[index]
 
-    if (index === newHopequotes.length - 1) {
-      clearInterval(timer)
-      console.log("now clearinterval")
-      // setTimeout(timer)
+    if (planet === "Tatooine") {
+      console.log("length", newHopequotes.length - 1)
+      if (index === newHopequotes.length - 1) {
+        console.log("first", index, planet)
+        index++
+        // clearInterval(timer)
+        console.log("now clearinterval")
+        // setTimeout(timer)
+      }
+    } else if (planet === "Hoth") {
+      if (index === empireQuotes.length - 1) {
+        console.log("third", index, planet)
+        index++
+      }
     } else {
+      console.log("third", index, planet)
       index++
     }
 
-    if(index > 3) {
-      index = 0
-    }
-
+  if (index > 3) {
+    index = 0
+    clearInterval(timer)
   }
 
   timer = setInterval(displayQuote, 2000)
+}
+
+  
+
+
 
 })
 
 
-const filmDataFunction = () => {
+function filmDataFunction() {
   
   fetch(apiURL + "people")
     .then((responseData) => {
@@ -146,40 +153,51 @@ const filmDataFunction = () => {
 
 // console.log(filmDataFunction())
 
-// populate planet-api-data with API data
+// populate #planet-api-data with API data for spinning globe gif element
 
-function planetWeather() {
-
+function planetWeather(e) {
   fetch(apiURL + "planets")
     .then((responseData) => {
       return responseData.json()
     })
     .then((parsedData) => {
+
       let spanEl = globeEl.querySelector("#planet-api-data")
-      console.log(imgs[0].getAttribute("planet"))
+      let planet = e.target.getAttribute("planet")
 
-      if(imgs[0].getAttribute("planet") === "Tatooine") {
-        console.log(spanEl)
-        spanEl.innerHTML = `Current weather on ${parsedData.results[0].name}<br>`
-        spanEl.innerHTML += `Current climate: ${parsedData.results[0].climate}<br>`
-        spanEl.innerHTML += `Current population: ${parsedData.results[0].population}<br>`
+      if (planet === "Tatooine") {
+        spanEl.innerHTML = `
+          <ul>
+          <li>${parsedData.results[0].name}</li>
+          <li>Climate: ${parsedData.results[0].climate}</li>
+          <li>Population: ${parsedData.results[0].population}</li>
+          </ul>`
+      } else if (planet === "Hoth") {
+        spanEl.innerHTML = `
+          <ul>
+          <li>${parsedData.results[3].name}</li>
+          <li>Climate: ${parsedData.results[3].climate}</li>
+          <li>Population: ${parsedData.results[3].population}</li>
+          </ul>`
+      } else {
+        spanEl.innerHTML = `
+          <ul>
+          <li>${parsedData.results[6].name}</li><li>
+          <li>Climate: ${parsedData.results[6].climate}</li>
+          <li>Population: ${parsedData.results[6].population}</li>
+          </ul>`
       }
 
-      if(imgs[1].getAttribute("planet") === "Hoth") {
-        console.log(parsedData.results[3].name)
-        console.log(`Current climate: ${parsedData.results[0].climate} \n`)
-        console.log(`Current population: ${parsedData.results[0].population}`)
-        console.log(spanEl.textContent)
-      }
-      
-      if(imgs[2].getAttribute("planet") === "Endor") {
-        console.log(parsedData.results[6].name)
-        console.log(`Current climate: ${parsedData.results[0].climate} \n`)
-        console.log(`Current population: ${parsedData.results[0].population}`)
-        console.log(spanEl.textContent)
-      }
     })
-    
 }
 
-planetWeather()
+
+function changeBackground() {
+  let body = document.body
+  body.style.backgroundColor = "red"
+}
+
+
+
+
+
