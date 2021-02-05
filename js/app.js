d90e26ce6a1abb8abac74ab8fe9014e6bd1b11ec
cmendoza <clrwxb@gmail.com>
2021-02-04 17:57:42 -0800

    Readme.md github file add on again and again

--- js/app.js
+++ js/app.js
@@ -28,21 +28,21 @@ fetch(apiURL + "planets")
       imgs[i].addEventListener("click", function (e) {
         let planet = e.target.getAttribute("planet")
         if (planet === "Tatooine") {
-          titleDataEl.innerHTML = ` Also known as the planet of the twin suns. This is the home planet of the Skywalker bloodline`          
+          titleDataEl.innerHTML = ` Also known as the planet of the twin suns. This is the home planet of the Skywalker bloodline`
         } else if (planet === "Hoth") {
           titleDataEl.innerHTML = `This planet made it's debut in the second installment of the original star wars trilogy, known for its freezing cold weather, beautiful views and Taun-Tauns!`
         } else {
           titleDataEl.innerHTML = `Home of tall trees, tree-house structures, and of course home of the furry Ewok's!`
-          
+
         }
       })
     }
   })
 
-  for(let i = 0; i < imgs.length; i++) {
-    imgs[i].addEventListener("click", planetWeather)
-  }
-  
+for (let i = 0; i < imgs.length; i++) {
+  imgs[i].addEventListener("click", planetWeather)
+}
+
 
 
 // Functions
@@ -55,7 +55,7 @@ document.querySelector("#thumbs").addEventListener("click", (e) => {
 
   function displayQuote() {
 
-    
+
     const newHopequotes = ["That’s no moon. It’s a space station. — Obi-Wan Kenobi", "Aren’t you a little short for a stormtrooper? — Leia Organa", "I find your lack of faith disturbing. — Darth Vader"]
 
     const empireQuotes = ["Yes, Your Highnessness? – Han Solo", "He’s no good to me dead. – Boba Fett", "The son of Skywalker must not become a Jedi. – Emperor Palpatine",
@@ -63,50 +63,43 @@ document.querySelector("#thumbs").addEventListener("click", (e) => {
 
     const returnQuotes = ["Bring me Solo and the Wookiee. They will all suffer for this outrage.", "Obi-Wan has taught you well.", "And now, young Skywalker...you will die.", "IT'S A TRAP!!", "You cannot hide forever, Luke.", "I am a Jedi, like my father before me", "What is thy bidding, my master?"]
 
-    
+
     let planet = e.target.getAttribute("planet")
 
-    
+
     if (planet === "Tatooine") {
 
-      if(index === newHopequotes.length - 1) {
-        console.log("if", "index", index)
+      if (index === newHopequotes.length - 1) {
         quotesEl.innerHTML = newHopequotes[index]
         clearInterval(timer)
       } else {
         quotesEl.innerHTML = newHopequotes[index]
-        console.log("inside tat else", "index", index)
         index++
       }
 
-      if(index > newHopequotes.length-1) {
+      if (index > newHopequotes.length - 1) {
         index = 0
       }
 
-    } else if(planet === "Hoth") {
-
-      if(index === empireQuotes.length - 1) {        
+    } else if (planet === "Hoth") {
+      if (index === empireQuotes.length - 1) {
         clearInterval(timer)
       } else {
         quotesEl.innerHTML = empireQuotes[index]
-        console.log("inside empire else", index)
         index++
-      } 
+      }
 
-      if(index > empireQuotes.length-1) {
+      if (index > empireQuotes.length - 1) {
         index = 0
       }
 
-    } else if(planet === "Endor") {
-      
+    } else if (planet === "Endor") {
+
       quotesEl.innerHTML = returnQuotes[index]
-      console.log("inside return else", index)
-      console.log(index, returnQuotes[index])
       index++
       clearInterval(timer)
 
-
-      if (index > returnQuotes.length-1) {
+      if (index > returnQuotes.length - 1) {
         index = 0
       }
     }
@@ -127,7 +120,7 @@ function planetWeather(e) {
     })
     .then((parsedData) => {
 
-      
+
       let planet = e.target.getAttribute("planet")
 
       if (planet === "Tatooine") {
